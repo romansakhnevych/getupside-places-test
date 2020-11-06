5e8e9e1da09bf432fccedd9ff120ef47ad27d9a9
Roman Sakhnievych <roman.sakhnievych@glaballogic.com>
2020-11-06 18:10:39 +0200

    Project setup

---
+++ getupside-places-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  getupside-places-test
+//
+//  Created by Roman Sakhnevych on 06.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
